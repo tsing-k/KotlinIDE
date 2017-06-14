7e8eec98c300ecb28fec3c417bc362c8a0839c5a
qdu <onlylinker@126.com>
2017-06-14 23:32:31 +0800

    设置utf-8编码

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
+
     MainWindow w;
     w.show();
 
